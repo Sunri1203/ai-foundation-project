83e9ab5eeac938b4e92a6efe5839c2861ede842c
Sunri <rtyu3145@gmail.com>
2025-08-28 13:09:52 +0800

    LLM data struction test

--- src/dataio.py
+++ src/dataio.py
@@ -7,3 +7,19 @@ model = AutoModelForCausalLM.from_pretrained(model_id)
 
 print(Token)
 print(model)
+
+text = "你好世界"
+
+token_slow_list = Token(text)
+print(token_slow_list)
+
+token_slow = {
+    "input_ids": torch.tensor([token_slow_list.input_ids]),
+    "attention_mask" : torch.tensor([token_slow_list.attention_mask])
+}
+
+print("token_slow:", token_slow)
+token_fast = Token(text , return_tensors = "pt")
+# return_tensors="pt" 是告訴 tokenizer：把輸出資料直接轉成 PyTorch tensor 格式
+# 其他格式"tf"：TensorFlow tensor ,"np"：NumPy array
+print("token_fast:", token_fast)
